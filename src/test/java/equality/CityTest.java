e1200e2a19bb9b6ba0e5a36cea01da9aaf09a508
B T Forsythe <brian@btforsythe.io>
2018-01-31 11:39:21 -0500

    equal to equivalent

--- src/test/java/equality/CityTest.java
+++ src/test/java/equality/CityTest.java
@@ -6,7 +6,9 @@ import org.junit.Test;
 
 /**
  * These tests are named oddly to facilitate the exercise. Make them pass in
- * order, one at a time in the simplest fashion.
+ * order, one at a time, in the simplest fashion.
+ * 
+ * City is a <em>value object</em>, so we define equality based on state (attribute values).
  */
 public class CityTest {
 
@@ -18,5 +20,12 @@ public class CityTest {
 	@Test
 	public void _01_shouldBeEqualToItself() {
 		assertTrue(underTest.equals(underTest));
+	}
+	
+	@Test
+	public void _02_shouldBeEqualToCityWithSameValues() {
+		City equivalent = new City("Columbus", "OH");
+		
+		assertTrue(underTest.equals(equivalent));
 	}
 }
