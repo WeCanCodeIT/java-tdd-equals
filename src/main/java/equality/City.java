2b3a31c6deabf10451a640148d613d26989bdb88
B T Forsythe <brian@btforsythe.io>
2018-01-31 11:56:45 -0500

    solution

--- src/main/java/equality/City.java
+++ src/main/java/equality/City.java
@@ -2,7 +2,29 @@ package equality;
 
 public class City {
 
+	private String name;
+	private String state;
+
 	public City(String name, String state) {
+		this.name = name;
+		this.state = state;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+		if(!(obj instanceof City)) {
+			return false;
+		}
+		City other = (City) obj;
+		return other.name.equals(name) && other.state.equals(state);
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
 	}
 
 }
